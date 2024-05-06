f7046279ecf9814b2b2aa7ab0b91f2692a9da8af
shadabtanjeed <stanjeed@gmail.com>
2024-05-06 21:09:58 +0600

    medication back button always takes to homepage

--- elder_ring/lib/Medication Reminder/medication_schedule.dart
+++ elder_ring/lib/Medication Reminder/medication_schedule.dart
@@ -5,6 +5,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'medicine_database.dart';
 import 'package:intl/intl.dart';
+import 'package:elder_ring/home_page.dart';
 
 class MedicationSchedule extends StatefulWidget {
   const MedicationSchedule({super.key});
@@ -239,44 +240,56 @@ class _MedicationScheduleState extends State<MedicationSchedule> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text(
-          'Medication Schedule',
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
-        ),
-        backgroundColor: const Color(0xFF2798E4),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
-      ),
-      body: Container(
-        margin: const EdgeInsets.only(top: 30, left: 20, right: 20),
-        child: allMedicineDetails(),
-      ),
-      floatingActionButton: Container(
-        width: 75.0,
-        child: FloatingActionButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const AddMedicine()),
-            );
-          },
+    return WillPopScope(
+      onWillPop: () async {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'Medication Schedule',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+          ),
           backgroundColor: const Color(0xFF2798E4),
-          child: const Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.add, color: Colors.white),
-              Text('Add',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 17,
-                      fontWeight: FontWeight.w600)),
-            ],
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back, color: Colors.white),
+            onPressed: () => Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => HomePage()),
+            ),
+          ),
+        ),
+        body: Container(
+          margin: const EdgeInsets.only(top: 30, left: 20, right: 20),
+          child: allMedicineDetails(),
+        ),
+        floatingActionButton: Container(
+          width: 75.0,
+          child: FloatingActionButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const AddMedicine()),
+              );
+            },
+            backgroundColor: const Color(0xFF2798E4),
+            child: const Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.add, color: Colors.white),
+                Text('Add',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 17,
+                        fontWeight: FontWeight.w600)),
+              ],
+            ),
           ),
         ),
       ),
