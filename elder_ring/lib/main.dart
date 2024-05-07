4367394c50b2354d0bdc9d8057d31c50f1e4e046
shadabtanjeed <stanjeed@gmail.com>
2024-05-08 02:20:13 +0600

    Revert "change theme added"
    This reverts commit 497ed3cd99065731f62df52e7212ae3b4f6f52a4.

--- elder_ring/lib/main.dart
+++ elder_ring/lib/main.dart
@@ -1,24 +1,15 @@
 import 'package:elder_ring/main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:elder_ring/theme.dart';
-import 'package:elder_ring/theme_provider.dart'; // Import the ThemeProvider
-import 'package:provider/provider.dart';
-import 'theme_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => ThemeProvider(Lightmode),
-      child: const MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
@@ -26,7 +17,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: const MainPage(),
-      theme: Provider.of<ThemeProvider>(context).themeData,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xFF2798E4),
+        fontFamily: 'Jost', // Use the Jost font
+      ),
     );
   }
 }
